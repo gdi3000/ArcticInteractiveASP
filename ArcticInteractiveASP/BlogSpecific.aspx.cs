32a96347d3e68d7ee797a2fc9d4ad1825d4f443a
gdi3000 <dani056i@edu.easj.dk>
2019-04-03 09:05:59 +0200

    Ansvar fra BlogSpecifc.aspx.cs til BlogSpecificDB.cs

--- ArcticInteractiveASP/BlogSpecific.aspx.cs
+++ ArcticInteractiveASP/BlogSpecific.aspx.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+﻿using ArcticInteractiveASP.Database;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -26,29 +27,12 @@ namespace ArcticInteractiveASP
 
         private BlogPost LoadSingleBlog(string id)
         {
-            BlogPost blogpost = new BlogPost();
-            string query = "SELECT * FROM Blog WHERE IdBlog =?id";
-            using (MySqlConnection connection = new MySqlConnection())
-            {
-                connection.ConnectionString = "Server=sql7.freemysqlhosting.net;Database=sql7285794;Uid=sql7285794;Pwd=mjbGqW4ly4;";
-                connection.Open();
-                MySqlCommand command = new MySqlCommand(query, connection);
-                command.Parameters.Add(new MySqlParameter("id", id));
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        
-                        
-                        blogpost.IdBlog = (int)reader["IdBlog"];
-                        blogpost.Date = reader["Date"].ToString();
-                        blogpost.Text = reader["Text"].ToString();
-                        blogpost.Title = reader["Title"].ToString();
-                        
-                    }
-                }
-            }
-            return blogpost;
+
+            BlogSpecificDB dB = new BlogSpecificDB();
+
+           return dB.LoadSingleBlog(id);
+
+           
         }
 
         public List<BlogPost> BlogPostlist
--- ArcticInteractiveASP/Database/BlogSpecificDB.cs
+++ ArcticInteractiveASP/Database/BlogSpecificDB.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,33 @@ namespace ArcticInteractiveASP.Database
 {
     public class BlogSpecificDB
     {
+
+        public BlogPost LoadSingleBlog(string id)
+        {
+            BlogPost blogpost = new BlogPost();
+            string query = "SELECT * FROM Blog WHERE IdBlog =?id";
+            using (MySqlConnection connection = new MySqlConnection())
+            {
+                connection.ConnectionString = "Server=sql7.freemysqlhosting.net;Database=sql7285794;Uid=sql7285794;Pwd=mjbGqW4ly4;";
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.Add(new MySqlParameter("id", id));
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+
+
+                        blogpost.IdBlog = (int)reader["IdBlog"];
+                        blogpost.Date = reader["Date"].ToString();
+                        blogpost.Text = reader["Text"].ToString();
+                        blogpost.Title = reader["Title"].ToString();
+
+                    }
+                }
+            }
+            return blogpost;
+        }
 
     }
 }

0956af9ff8b326c2a0c4d951acb64a75a20e7e13
gdi3000 <dani056i@edu.easj.dk>
2019-04-08 09:37:12 +0200

    Test Pushing

--- ArcticInteractiveASP/Blog.aspx.cs
+++ ArcticInteractiveASP/Blog.aspx.cs
@@ -29,33 +29,7 @@ namespace ArcticInteractiveASP
 
             return blogDB.LoadFromDB();
 
-            
-
-            //List<BlogPost> list = new List<BlogPost>();
-
-
-            //string query = "SELECT * FROM Blog";
-            //using (MySqlConnection connection = new MySqlConnection())
-            //{
-            //    connection.ConnectionString = "Server=sql7.freemysqlhosting.net;Database=sql7285794;Uid=sql7285794;Pwd=mjbGqW4ly4;";
-                
-            //    connection.Open();
-            //    MySqlCommand command = new MySqlCommand(query, connection);
-            //    using (var reader = command.ExecuteReader())
-            //    {
-            //        while (reader.Read())
-            //        {
-            //            BlogPost blogpost = new BlogPost();
-            //            blogpost.IdBlog = (int)reader["IdBlog"];
-            //            blogpost.Date = reader["Date"].ToString();
-            //            blogpost.Text = reader["Text"].ToString();
-            //            blogpost.Title = reader["Title"].ToString();
-            //            list.Add(blogpost);
-            //        }
-            //    }
-            //}
-
-            //return list;
+  
 
         }
 
